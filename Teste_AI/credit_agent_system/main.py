855fe75b281095bfbc0c39de5950a18386674899
AndreKoba <101141467+AndreKoba@users.noreply.github.com>
2025-12-01 18:29:53 -0300

    Update main
    Adicionando comentários

--- Teste_AI/credit_agent_system/main.py
+++ Teste_AI/credit_agent_system/main.py
@@ -1,14 +1,14 @@
 import os
 import sys
 
-# Add the project root to sys.path to ensure imports work correctly
+# Adiciona a raiz do projeto ao sys.path para garantir que as importações funcionem corretamente
 current_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(current_dir)
 
 from agents.triage_agent import TriageAgent
 
 def main():
-    # Path to the CSV file
+    # Caminho para o arquivo CSV
     data_path = os.path.join(current_dir, 'data', 'clientes.csv')
     
     agent = TriageAgent(data_path)
